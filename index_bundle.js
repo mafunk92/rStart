bef5486147bc6fa8027c3caaa41b23925c0c014a
Michael Funk <mafunk92@gmail.com>
2016-08-18 20:09:21 -0500

    Updates

--- index_bundle.js
+++ index_bundle.js
@@ -72,7 +72,7 @@
 
 	    var Httpreq = new XMLHttpRequest(); // a new request
 	    Httpreq.open("GET", url, true);
-	    Httpreq.setRequestHeader("Accept", "application/json");
+	    Httpreq.setRequestHeader("dataType", "jsonp");
 	    Httpreq.send(null);
 
 	    var json_obj = JSON.parse(Httpreq.response);
